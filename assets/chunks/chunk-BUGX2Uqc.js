4d55b63cdbbf999c8d862fdcbd757136410b4b05
teamcrablegs <teamcrablegs@chynes.com>
2026-09-05 12:38:21 -0700

    Initial Webstudio commit

---
+++ assets/chunks/chunk-BUGX2Uqc.js
@@ -0,0 +1,1 @@
+import{a as e,h as s}from"./chunk-D6r3HFbW.js";const m="div",n=e.forwardRef(({tag:a,...r},o)=>{const t=s(r)??a??m;return e.createElement(t,{...r,ref:o})});n.displayName="Box";export{n};
